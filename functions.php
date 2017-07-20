f165c1e23f1ddeffc668a7c216ffb1805564df50
Alex Santos <asantos@observador.pt>
2017-07-20 22:36:27 +0100

    new grid

--- functions.php
+++ functions.php
@@ -125,9 +125,7 @@ function alexsantos_scripts() {
    
    	wp_enqueue_style( 'muitoestilo' );
 
-	wp_enqueue_script( 'alexsantos-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'alexsantos-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+   	wp_enqueue_script( 'alexsantos-script', get_template_directory_uri() . '/build/alexsantos.min.js', array('jquery'), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- index.php
+++ index.php
@@ -17,8 +17,57 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<div class="container">
+<div class="grid">
+  <div class="col-1-4">
+  	 * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists. sdsdd sdsd dsd s d sd sd sdsd ssdds dsds  s sd ds ds sd s d
+  </div>
+  <div class="col-1-4">
+  	 * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+  </div>
+  <div class="col-1-4">
+  	 * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+  </div>
+  <div class="col-1-4">
+  	 * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+  </div>
+</div>
 
+
+<div class="grid">
+   <div class="col-1-3">
+  	 * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+  </div>
+  <div class="col-1-3">
+  	<div>
+  	 * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.</div>
+  </div>
+  <div class="col-1-3">
+  	 * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+  </div>
+</div>
+
+<div class="grid">
 		<?php
 		if ( have_posts() ) :
 
@@ -29,8 +78,6 @@ get_header(); ?>
 
 			<?php
 			endif; ?>
-
-			<div class="container">
 			
 			<?php 
 			/* Start the Loop */
@@ -54,7 +101,7 @@ get_header(); ?>
 
 		endif; ?>
 
-			</div>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
